3d4f0ab3826b720efff0d9fe6d7c70e2bac6abff
cagla997 <cagla.sener2011@hotmail.com>
2026-01-11 22:53:20 +0300

    Create PrimeNumber.java
    Prime Number Java Program

---
+++ Activity 04/PrimeNumber.java
@@ -0,0 +1,44 @@
+package asalsayi;
+
+import java.util.Scanner;
+
+public class AsalSayi {
+	public static void main(String[] args) {
+		
+		Scanner oku = new Scanner(System.in);
+		System.out.print("Lower Bound:");
+		int s1; 
+		s1 = oku.nextInt();
+		
+		System.out.print("Upper Bound:");
+		int s2;
+		s2 = oku.nextInt();
+		
+		System.out.println("Prime number in the Inclusive Range [" + s1 + "," + s2 + "]");
+		System.out.println("-----------------------------------------");
+		
+		
+		
+		for (int num = s1; num <= s2; num++) { //num kullanıcının verdiği değerler arasındaki sayılar
+			boolean asal = true;
+			if (num <= 1) {
+				asal = false;
+			}
+			else
+				for (int i = 2; i < num; i++) { //i bölünüyor mu diye kontrol ettiğimiz sayılar
+					if (num % i == 0) {
+						asal = false;
+					}
+					
+				}
+			
+			if (asal) {
+				System.out.print(num + " ");
+			}
+			
+			
+		}
+		
+	}
+
+}
